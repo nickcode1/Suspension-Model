2f66e6c30b67643134a79f002c11c633eb84003f
Nick Harrison <nickgan1012@gmail.com>
2026-07-06 22:33:46 -0300

    done for the night

---
+++ Full_Car.py
@@ -0,0 +1,64 @@
+import math
+import matplotlib.pyplot as plt
+
+
+def road(x):
+    """Dirt road height (m) at position x (m). Replaces the random h_Wheel."""
+    # Gently rolling base surface, a few cm of undulation
+    h = (3
+         + 0.06 * math.sin(0.4 * x)
+         + 0.03 * math.sin(1.3 * x + 2.0)
+         + 0.015 * math.sin(3.1 * x + 0.7))
+    # Small rocks (bumps) and potholes (dips) at deterministic spots
+    cell = math.floor(x / FEATURE_SPACING)
+    for c in (cell - 1, cell, cell + 1):
+        if _rand(c) < 0.35:
+            continue  # smooth stretch, no feature here
+        center = (c + 0.15 + 0.7 * _rand(c, 1)) * FEATURE_SPACING
+        if _rand(c, 2) < 0.55:  # rock
+            amp = 0.03 + 0.05 * _rand(c, 3)
+            half_width = 0.15 + 0.15 * _rand(c, 4)
+        else:  # pothole
+            amp = -(0.06 + 0.09 * _rand(c, 3))
+            half_width = 0.35 + 0.35 * _rand(c, 4)
+        d = x - center
+        if abs(d) < half_width:
+            h += amp * 0.5 * (1 + math.cos(math.pi * d / half_width))
+    return h
+
+
+
+
+l  = 1
+L1 = 1
+L2 = 1
+
+
+
+k1 = 1
+k2 = 1
+k3 = 1
+k4 = 1
+
+c1 = 1
+c2 = 1
+c3 = 1
+c4 = 1
+
+zu1 = []
+zu2 = []
+zu3 = []
+zu4 = []
+
+zs = []
+
+phiS = []
+thetaS = []
+
+dt = 0.01
+t = 0
+
+
+
+
+    

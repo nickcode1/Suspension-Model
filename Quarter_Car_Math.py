6e97958dfc246c17f2c7b1a914f00af04c30a9a8
NickH <nickgan1012@gmail.com>
2026-07-09 17:41:55 -0300

    removed mg

--- Quarter_Car_Math.py
+++ Quarter_Car_Math.py
@@ -8,22 +8,22 @@ h_Wheel = [0,0]  # Wheel height history
 g = 9.81  # Gravity
 dt = .01  # Time step
 t = 0  # Initial time
-M_car = 1500  # kg
-C1 = 150  # N/m
-K1 = 1500  # N.s/m
+M_car = 1500/4  # kg
+C1 = 2000  # N/m
+K1 = 15000  # N.s/m
 
 # Simulation loop
-while t<=100:
+while t<=10:
 
    
-    h_Wheel.append(random.uniform(1,5))
+    h_Wheel.append(random.uniform(-.05,.05))
 
-    h_Car.append(((2*M_car + C1*dt)*h_Car[-1] - M_car*h_Car[-2] + C1*(h_Wheel[-1]-h_Wheel[-2])*dt + K1*h_Wheel[-1]*dt**2 - M_car*g*dt**2)/(M_car+C1*dt+K1*dt**2))
+    h_Car.append(((2*M_car + C1*dt)*h_Car[-1] - M_car*h_Car[-2] + C1*(h_Wheel[-1]-h_Wheel[-2])*dt + K1*h_Wheel[-1]*dt**2)/(M_car+C1*dt+K1*dt**2))
    
     t+=dt
 
-plt.plot(range(0, 10002), h_Wheel, label='Wheel Height')
-plt.plot(range(0, 10002), h_Car, label='Car Height')
+plt.plot(range(0, 1003), h_Wheel, label='Wheel Height')
+plt.plot(range(0, 1003), h_Car, label='Car Height')
 plt.legend()                                
 plt.show()
 
